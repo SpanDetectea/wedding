5e864e76547f82197e2bdeafda32cc9be9c54f4b
Span <spandetectea@gmail.com>
2024-02-24 12:37:45 +0300

    календарь удален

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,7 +4,14 @@ import calendar from "../../assets/images/calendar2.png";
 const Calendar = () => {
   return (
     <div className="calendar">
-      <img src={calendar} alt="" className="calendar__image" />
+      {/* <img src={calendar} alt="" className="calendar__image" /> */}
+      <div className="container">
+        <div className="Month">июнь</div>
+        <div className="year">2024</div>
+        <div className="time stripes">16:00</div>
+        <div className="number">22</div>
+        <div className="dayOfWeek stripes">Суббота</div>
+      </div>
     </div>
   );
 };
