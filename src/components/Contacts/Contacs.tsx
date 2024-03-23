567651af2c0b99284d2be415674dde18e1f4236e
Span <spandetectea@gmail.com>
2024-03-23 17:46:50 +0300

    исправление фотографии жениха и невесты в разделе контакты

--- src/components/Contacts/Contacs.tsx
+++ src/components/Contacts/Contacs.tsx
@@ -1,6 +1,7 @@
 import "./Contacs.scss";
 import groom from "../../assets/images/groom.png";
 import bride from "../../assets/images/bride.png";
+import we from '../../assets/images/we.png'
 
 const Contacs = () => {
   return (
@@ -10,7 +11,7 @@ const Contacs = () => {
         Если у вас возникнут вопросы, вы всегда сможете позвонить одному из нас
       </p>
       <div className="contacs__wrapper">
-        <div className="contacs__wrapper__info">
+        {/* <div className="contacs__wrapper__info">
           <div className="contacs__wrapper__info__number">
             <div>Жених:</div>
             <div>+79114274286</div>
@@ -23,6 +24,21 @@ const Contacs = () => {
             <div>+79004602093</div>
           </div>
           <img src={bride} alt="" className="contacs__wrapper__info__image" />
+        </div> */}
+        <div className="contacs__wrapper__info">
+          <div className="contacs__wrapper__info__number">
+            <div>Роман:</div>
+            <div>89114274286</div>
+          </div>
+        </div>
+        <div className="contacs__wrapper__we">
+          <img src={we} alt="" className="contacs__wrapper__we__photo"/>
+        </div>
+        <div className="contacs__wrapper__info">
+          <div className="contacs__wrapper__info__number">
+            <div>Екатерина:</div>
+            <div>89004602093</div>
+          </div>
         </div>
       </div>
       <div></div>
